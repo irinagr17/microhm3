0fe42674c078f3f8eb12bfbcccf332d22d998435
irinagr17 <115007394+irinagr17@users.noreply.github.com>
2022-11-24 21:39:52 +0800

    Add files via upload

--- sketch_nov2c.ino
+++ sketch_nov2c.ino
@@ -1,7 +1,6 @@
 #define led_pin A2
 
 int count=0;
-// int arr = [];
 
 void setup() {
   Serial.begin(9600);
@@ -13,25 +12,4 @@ void loop() {
   val = map(val, 0, 1023, 0, 100);
   delay(100);
   Serial.print(val);
-  // while(true){
-  //   if(val / 10 == 0){
-  //     break;
-  //   }
-  //   val = val / 10;
-  //   count++;
-  // }
-  //delay(1000);
-  // Serial.print(count);
-  //Serial.println(val);
-  // while (Serial.available()){
-  //   s = Serial.read();
-  // }
-  
-  // if( s == '1'){
-  //   digitalWrite(led_pin, HIGH);
-  // }
-  
-  // else if(s == '0'){
-  //   digitalWrite(led_pin, LOW);
-  // }
 }
