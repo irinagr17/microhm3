561c173c86383f41a6dc1f27896bdc0ddf2cb270
irinagr17 <115007394+irinagr17@users.noreply.github.com>
2022-12-21 00:12:53 +0800

    Add files via upload

---
+++ Range.h
@@ -0,0 +1,19 @@
+const int trig_pin = 4; //D2 на плате
+const int echo_pin = 5; //D1 на плате
+
+void init_sensor(){
+  pinMode(trig_pin, OUTPUT);
+  pinMode(echo_pin, INPUT);
+  digitalWrite(trig_pin, LOW);
+}
+
+float get_range(){
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin, LOW);
+  long duration = pulseIn(echo_pin, HIGH);
+  float distance = duration / 28.5;
+  return distance;
+}
