34055536ede8259257ca85aa70e1e8f8e9f43b12
irinagr17 <115007394+irinagr17@users.noreply.github.com>
2022-12-21 08:59:08 +0800

    Update sketch_nov2b.ino

--- morse_hw1/sketch_nov2b/sketch_nov2b.ino
+++ morse_hw1/sketch_nov2b/sketch_nov2b.ino
@@ -1,66 +1,62 @@
-#define DATA_LEVEL LOW
-#define SPACE_LEVEL HIGH
-#define DASH_DURATION 3
-#define DOT_DURATION 1
-#define TU 100
-#define DATA_PIN 9
+#define DATA_LEVEL LOW 
+#define SPACE_LEVEL HIGH 
+#define DASH_DURATION 3 
+#define DOT_DURATION 1 
+#define TU 100 
+#define DATA_PIN 9 
 
-// #define SPACE false //красное
-// #define DATA true
-// long start_data, start_space;
-// long duration[20];
-// bool color[20];
-// int index = 0;
-// int previous = SPACE_LEVEL;
+//#define SPACE false //красное 
+//#define DATA true 
+//long start_data, start_space; 
+//long duration[20]; 
+//bool color[20]; 
+//int index = 0; 
+//int previous = SPACE_LEVEL; 
 
 String CODES[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
 char LETTERS[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 int NLETTERS = 27;
 
-// String CODES[] = {".-", "--.."};
-// char LETTERS[] = {'A', 'Z'};
-// int NLETTERS = 2;
+void setup() { 
+  pinMode(DATA_PIN, OUTPUT); 
+  digitalWrite(DATA_PIN, SPACE_LEVEL); 
+  Serial.begin(9600); 
+} 
 
-void setup() {
-  pinMode(DATA_PIN, OUTPUT);
-  digitalWrite(DATA_PIN, SPACE_LEVEL);
-  Serial.begin(9600);
-}
-
-void loop() {
-  //available() показывает сколько символов пришло
-  if(Serial.available() >= 1){
+void loop() { 
+//available() показывает сколько символов пришло 
+  if(Serial.available() >= 1){ 
     char letter = Serial.read();
     if(letter == ' '){
       send_word();
     }
-    else{
-      for(int iletter; iletter<NLETTERS; iletter++){
-        if(letter == LETTERS[iletter]){
-          String code = CODES[iletter];
-          send_letter(code);
-        }
+    else{ 
+      for(int iletter; iletter<NLETTERS; iletter++){ 
+        if(letter == LETTERS[iletter]){ 
+          String code = CODES[iletter]; 
+          send_letter(code); 
+        } 
       }
     }
-  }
-}
+  } 
+} 
 
-void send_letter(String code){
-  int code_len = code.length();
-        for(int i=0; i<code_len; i++){
-          char symbol = code[i];
-          if(symbol == '.'){
-            digitalWrite(DATA_PIN, DATA_LEVEL);
-            delay(TU);
-          }
-          if(symbol == '-'){
-            digitalWrite(DATA_PIN, DATA_LEVEL);
-            delay(DASH_DURATION*TU);
-          }
-          digitalWrite(DATA_PIN, SPACE_LEVEL);
-          delay(TU);
-        }
-        delay((DASH_DURATION - 1)* TU );
+void send_letter(String code){ 
+  int code_len = code.length(); 
+  for(int i=0; i<code_len; i++){ 
+    char symbol = code[i]; 
+    if(symbol == '.'){ 
+      digitalWrite(DATA_PIN, DATA_LEVEL); 
+      delay(TU); 
+    } 
+    if(symbol == '-'){ 
+      digitalWrite(DATA_PIN, DATA_LEVEL); 
+      delay(DASH_DURATION*TU); 
+    } 
+    digitalWrite(DATA_PIN, SPACE_LEVEL); 
+    delay(TU); 
+  } 
+  delay((DASH_DURATION - 1)* TU ); 
 }
 
 void send_word(){
