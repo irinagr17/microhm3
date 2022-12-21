3b2c11b93c905ff2a7b6cd7e5073d1e4b737b2d9
irinagr17 <115007394+irinagr17@users.noreply.github.com>
2022-12-21 08:59:55 +0800

    Update sketch_nov2a.ino

--- morse_hw1/sketch_nov2a/sketch_nov2a.ino
+++ morse_hw1/sketch_nov2a/sketch_nov2a.ino
@@ -5,6 +5,8 @@
 #define DATA true
 #define DASH_DURATION 3
 #define DOT_DURATION 1
+#define LETTER_DURATION 3
+#define WORD_DURATION 7	
 #define TU 100
 
 long start_data, start_space;
@@ -17,61 +19,82 @@ String CODES[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","
 char LETTERS[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 // String CODES[] = {".-", "--.."};
 // char LETTERS[] = {'A', 'Z'};
-int NLETTERS = 26;
-
+int NLETTERS = 27; 
+String message = "";
+bool isEndWord = false;
 void setup() {
-  pinMode(DATA_PIN, INPUT);
   Serial.begin(9600);
+  pinMode(DATA_PIN, INPUT);
 }
-
 void loop() {
+  // put your main code here, to run repeatedly:
   fill_arrays();
   decode_letter();
-}
-void decode_letter(){
-  for(int i=0; i<index; i++)
-  {
-    if(duration[i] == DASH_DURATION and color[i] == SPACE)
-    {
-      String code = "";
-      for (int j=0; j<i; j++)
-      {
-        if(duration[j] == DASH_DURATION and color[j] == DATA)
-        {
-          code += "-";
-        }
-        else if(duration[j] == DOT_DURATION and color[j] == DATA)
-        {
-          code += ".";
-        }
-        duration[j] = 0;
-      }
-      duration[i] = 0;
-      for(int iletter=0; iletter<NLETTERS; iletter++)
-      {
-        if(code == CODES[iletter])
-        {
-          Serial.println(LETTERS[iletter]);
-        }
-      }
-    }
-  }
-  // index = 0;
+  
 }
 
-void fill_arrays(){
-  int current  = digitalRead(DATA_PIN);
-  if(current == DATA_LEVEL and previous == SPACE_LEVEL){
-    start_data = millis();
-    duration[index] = int((millis() - start_space + 0.5 * TU) / TU);
-    color[index] = SPACE;
-    index ++;
+
+void decode_letter(){ 
+  for(int i=0; i < index; i++){ 
+    bool isEditing = false;
+    if (duration[i] == DASH_DURATION and color[i] == SPACE){ 
+      String code = ""; 
+      for (int j=0; j < i; j++){ 
+          if (duration[j] == DASH_DURATION and color[j] == DATA){ 
+            code += '-'; 
+          } 
+          if (duration[j] == DOT_DURATION and color[j] == DATA){ 
+            code += '.'; 
+          } 
+          duration[j] = 0; 
+      } 
+      duration[i] = 0; 
+      for (int iletter=0; iletter < NLETTERS; iletter++){ 
+        if (code == CODES[iletter]){ 
+            isEditing = true;
+            message += LETTERS[iletter];
+            Serial.print(message);
+        } 
+      } 
+    }
+    if (isEditing && isEndWord)	
+    {
+      Serial.print("\n");	
+      index = 0;	
+      isEndWord = false;	
+    }
+  
+  if (isEditing)	
+  {
+  	index = 0;
+    message = "";
+  } }
+}
+
+
+void fill_arrays(){ 
+  int current = digitalRead(DATA_PIN); 
+  if (current == DATA_LEVEL and previous == SPACE_LEVEL){ 
+    start_data = millis(); 
+    duration[index] = int((millis() - start_space + 0.5 * TU) / TU); 
+    color[index] = SPACE; 
+    index++; 
+  } 
+  else if (current == SPACE_LEVEL and previous == DATA_LEVEL){ 
+    isEndWord = false;
+    start_space = millis(); 
+    duration[index] = int((millis() - start_data + 0.5 * TU) / TU); 
+    color[index] = DATA; 
+    index++; 
+  } 
+  else if (isEndWord == false && current == SPACE_LEVEL && previous == SPACE_LEVEL){	// Условие для обнаружения паузы (конца передачи данных) после передачи данных
+    long temp = millis() - start_space;	// фиксируем время
+    if (temp > TU * WORD_DURATION)	// Если время больше, чем длительность паузы
+    {
+    	isEndWord = true;
+    	duration[index] = WORD_DURATION;
+      	index++;	//  Сдвиг буфера, чтобы вывести последнюю букву
+  	}
   }
-  if(current == SPACE_LEVEL and previous == DATA_LEVEL){
-    start_space = millis();
-    duration[index] = int((millis() - start_data + 0.5 * TU) / TU);
-    color[index] = DATA;
-    index ++;
-  }
-  previous = current;
+  previous = current; 
 }
